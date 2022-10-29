e00bc8f80941da66d3aab847774c9ea08ca38e4d
helin <email@helin.com>
2022-10-29 12:40:35 +0300

    add category service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,7 @@ import { ProductlistComponent } from './components/productlist/productlist.compo
 
 @NgModule({
   declarations: [AppComponent, CalculatorComponent, ListviewComponent, ProductlistComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   exports: [],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/listview/listview.component.ts
+++ src/app/components/listview/listview.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CategoriesService } from './../../services/categories.service';
+import { Category } from 'src/app/models/category';
+
 @Component({
   selector: 'app-listview',
   templateUrl: './listview.component.html',
   styleUrls: ['./listview.component.css'],
 })
 export class ListviewComponent implements OnInit {
-  categories: string[] = [];
+  // ?: null olabilir demek.
+  // !: null olmayacak, bu property'i kullanmadan önce atama işlemini gerçekleştiriceğiz söz vermiş oluyoruz.
+  categories!: Category[];
   language: string = 'en';
-  constructor() {}
+
+  constructor(private categoriesService: CategoriesService) {}
 
   ngOnInit(): void {
     this.getCategories();
   }
 
   getCategories() {
-    this.categories = ['Gi', 'Ayakkabı', 'Parfüm', 'Yiyecek', 'İçecek'];
+    // Object tipi henüz belli olmayan referans tip diyebiliriz. Referans tiplerin en temel sınıfı diyebiliriz.
+    this.categoriesService.getCategories().subscribe((response) => {
+      this.categories = response;
+    });
   }
 }
