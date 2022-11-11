87c1e9537b93d7820c8de35250092f49a67f6744
Erdem Ceylan <erdemxceylan@gmail.com>
2022-11-11 05:09:23 +0300

    worked on styles VI

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -6,125 +6,124 @@ import { ServicesService } from 'src/app/services/service.service';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: 'services',
-  templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+   selector: 'services',
+   templateUrl: './cards.component.html',
+   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
 
-  services!: Services[];
-  searchText: string = '';
-  error: string = '';
-  addForm!: FormGroup;
-  addError: string = '';
-  editForm!: FormGroup;
-  editError: string = '';
-  isEditing: boolean = false;
-  editId!: number;
-  clonedServices: { [s: string]: Services; } = {};
+   services!: Services[];
+   searchText: string = '';
+   error: string = '';
+   addForm!: FormGroup;
+   addError: string = '';
+   editForm!: FormGroup;
+   editError: string = '';
+   isEditing: boolean = false;
+   editId!: number;
+   clonedServices: { [s: string]: Services; } = {};
    service2: Services[];
 
-  constructor(
-    private serviceService: ServicesService,
-    private formBuilder: FormBuilder,
-    private toastr: ToastrService
-  ) { }
+   constructor(
+      private serviceService: ServicesService,
+      private formBuilder: FormBuilder,
+      private toastr: ToastrService
+   ) { }
 
-  ngOnInit(): void {
-    this.getServices();
-    this.createForms();
-  }
+   ngOnInit(): void {
+      this.getServices();
+      this.createForms();
+   }
 
-  createForms() {
-    this.addForm = this.formBuilder.group({
-       name: ['', Validators.required],
-    });
-    this.editForm = this.formBuilder.group({
-       name: ['', Validators.required],
-    });
- }
+   createForms() {
+      this.addForm = this.formBuilder.group({
+         name: ['', Validators.required],
+      });
+      this.editForm = this.formBuilder.group({
+         name: ['', Validators.required],
+      });
+   }
 
-  getServices(): void {
-    this.serviceService.getServices().subscribe((response: Services[]) => {
-      // Observer Design Pattern
-      this.services = response;
-    });
-  }
+   getServices(): void {
+      this.serviceService.getServices().subscribe((response: Services[]) => {
+         // Observer Design Pattern
+         this.services = response;
+      });
+   }
 
-  add(): void {
-    if (this.addForm.invalid) {
-       this.toastr.warning('Lütfen bir servis ismi giriniz');
-       this.addError = 'Form is invalid';
-       return;
-    }
-    if (this.addError) this.addError = '';
+   add(): void {
+      if (this.addForm.invalid) {
+         this.toastr.warning('Lütfen bir servis ismi giriniz');
+         this.addError = 'Form is invalid';
+         return;
+      }
+      if (this.addError) this.addError = '';
 
-    const service: Services = {
-       ...this.addForm.value,
-    };
+      const service: Services = {
+         ...this.addForm.value,
+      };
 
-    this.serviceService.add(service).subscribe({
-       next: () => {
-          this.toastr.success(`${service.name} has been added.`);
-       },
-       error: (err) => {
-          console.log(err);
-          this.addError = err.statusText;
-          this.toastr.error(this.addError);
-          this.toastr.error(err.statusText);
-       },
-       complete: () => {
-          if (this.addError) this.addError = '';
-          this.addForm.reset();
-          this.getServices();
-       },
-    });
- }
+      this.serviceService.add(service).subscribe({
+         next: () => {
+            this.toastr.success(`${service.name} has been added.`);
+         },
+         error: (err) => {
+            console.log(err);
+            this.addError = err.statusText;
+            this.toastr.error(this.addError);
+            this.toastr.error(err.statusText);
+         },
+         complete: () => {
+            if (this.addError) this.addError = '';
+            this.addForm.reset();
+            this.getServices();
+         },
+      });
+   }
 
- edit(id: number) {
-  this.isEditing = true;
-  this.editId = id;
-  this.editForm.get('name')?.setValue(this.services.find(service => service.id === id)?.name);
-}
+   edit(id: number) {
+      this.isEditing = true;
+      this.editId = id;
+      this.editForm.get('name')?.setValue(this.services.find(service => service.id === id)?.name);
+   }
 
+   update(): void {
+      if (this.editForm.invalid || !this.editId) {
+         this.editError = 'Form is invalid';
+         return;
+      }
+      if (this.editError) this.editError = '';
 
-update(): void {
-  if (this.editForm.invalid || !this.editId) {
-     this.editError = 'Form is invalid';
-     return;
-  }
-  if (this.editError) this.editError = '';
+      const service: Services = {
+         id: this.editId,
+         ...this.editForm.value,
+      };
 
-  const service: Services = {
-     id: this.editId,
-     ...this.editForm.value,
-  };
+      this.serviceService.update(service).subscribe({
+         next: () => {
+            this.toastr.success('İşlem başarılı');
+         },
+         error: (err) => {
+            console.log(err);
+            this.editError = err.statusText;
+            this.toastr.error(this.editError);
+            this.toastr.error(err.statusText);
+         },
+         complete: () => {
+            if (this.editError) this.editError = '';
+            this.editForm.reset();
+            this.getServices();
+         },
+      });
 
-  this.serviceService.update(service).subscribe({
-     next: () => {
-        this.toastr.success('İşlem başarılı');
-     },
-     error: (err) => {
-        console.log(err);
-        this.editError = err.statusText;
-        this.toastr.error(this.editError);
-        this.toastr.error(err.statusText);
-     },
-     complete: () => {
-        if (this.editError) this.editError = '';
-        this.editForm.reset();
-        this.getServices();
-     },
-  });
+      this.isEditing = false;
+   }
 
-  this.isEditing = false;
-}
-
-delete(id: number) {
-  this.serviceService.delete(id).subscribe(() => {
-     this.getServices();
-  });
- }
+   delete(id: number) {
+      this.serviceService.delete(id).subscribe(() => {
+         this.getServices();
+      });
+   }
 
    onRowEditInit(service: Services) {
       this.clonedServices[service.id] = { ...service };
@@ -135,14 +134,14 @@ delete(id: number) {
    onRowEditSave(service: Services) {
       console.log('save');
       console.log(service);
-      
+
       if (service.name) {
          delete this.clonedServices[service.id];
       }
       else {
       }
    }
-   
+
    onRowEditCancel(service: Services, index: number) {
       console.log('editcancel');
       console.log(service, index);
