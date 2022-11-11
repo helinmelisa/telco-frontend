0dd0bde62c3f086f60fe11c924216971985aec3d
Erdem Ceylan <erdemxceylan@gmail.com>
2022-11-11 06:30:16 +0300

    homepage crud logic is complete

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -14,15 +14,9 @@ export class CardsComponent implements OnInit {
 
    services!: Services[];
    searchText: string = '';
-   error: string = '';
    addForm!: FormGroup;
-   addError: string = '';
-   editForm!: FormGroup;
-   editError: string = '';
-   isEditing: boolean = false;
-   editId!: number;
-   clonedServices: { [s: string]: Services; } = {};
-   service2: Services[];
+   displayAddForm: boolean = false;
+   clonedService: { [s: string]: Services } = {};
 
    constructor(
       private serviceService: ServicesService,
@@ -32,21 +26,17 @@ export class CardsComponent implements OnInit {
 
    ngOnInit(): void {
       this.getServices();
-      this.createForms();
+      this.createAddForm();
    }
 
-   createForms() {
+   createAddForm() {
       this.addForm = this.formBuilder.group({
-         name: ['', Validators.required],
-      });
-      this.editForm = this.formBuilder.group({
          name: ['', Validators.required],
       });
    }
 
    getServices(): void {
       this.serviceService.getServices().subscribe((response: Services[]) => {
-         // Observer Design Pattern
          this.services = response;
       });
    }
@@ -54,10 +44,8 @@ export class CardsComponent implements OnInit {
    add(): void {
       if (this.addForm.invalid) {
          this.toastr.warning('Lütfen bir servis ismi giriniz');
-         this.addError = 'Form is invalid';
          return;
       }
-      if (this.addError) this.addError = '';
 
       const service: Services = {
          ...this.addForm.value,
@@ -65,39 +53,25 @@ export class CardsComponent implements OnInit {
 
       this.serviceService.add(service).subscribe({
          next: () => {
-            this.toastr.success(`${service.name} has been added.`);
+            this.toastr.success(`${service.name} eklendi`);
          },
          error: (err) => {
             console.log(err);
-            this.addError = err.statusText;
-            this.toastr.error(this.addError);
             this.toastr.error(err.statusText);
          },
          complete: () => {
-            if (this.addError) this.addError = '';
+            this.displayAddForm = false;
             this.addForm.reset();
             this.getServices();
          },
       });
    }
 
-   edit(id: number) {
-      this.isEditing = true;
-      this.editId = id;
-      this.editForm.get('name')?.setValue(this.services.find(service => service.id === id)?.name);
-   }
-
-   update(): void {
-      if (this.editForm.invalid || !this.editId) {
-         this.editError = 'Form is invalid';
+   update(service: Services): void {
+      if(!service.name){
+         this.toastr.warning('Lütfen bir servis ismi giriniz');
          return;
       }
-      if (this.editError) this.editError = '';
-
-      const service: Services = {
-         id: this.editId,
-         ...this.editForm.value,
-      };
 
       this.serviceService.update(service).subscribe({
          next: () => {
@@ -105,51 +79,44 @@ export class CardsComponent implements OnInit {
          },
          error: (err) => {
             console.log(err);
-            this.editError = err.statusText;
-            this.toastr.error(this.editError);
             this.toastr.error(err.statusText);
-         },
-         complete: () => {
-            if (this.editError) this.editError = '';
-            this.editForm.reset();
-            this.getServices();
-         },
+            this.toastr.error('işlem başarısız');
+         }
       });
-
-      this.isEditing = false;
    }
 
-   delete(id: number) {
-      this.serviceService.delete(id).subscribe(() => {
-         this.getServices();
+   delete(service: Services) {
+      this.serviceService.delete(service.id).subscribe({
+         next: () => this.toastr.success(`${service.name} silindi`),
+         error: (err) => {
+            console.log(err);
+            this.toastr.error(err.statusText);
+            this.toastr.error('işlem başarısız');
+         },
+         complete: () => this.getServices()
       });
+   }
+
+   onAdd() {
+      this.displayAddForm = !this.displayAddForm;
+   }
+
+   onAddCancel() {
+      this.displayAddForm = false;
    }
 
    onRowEditInit(service: Services) {
-      this.clonedServices[service.id] = { ...service };
-      console.log('editinit');
-      console.log(service);
+      this.clonedService[service.id] = { ...service };
    }
 
    onRowEditSave(service: Services) {
-      console.log('save');
-      console.log(service);
-
-      if (service.name) {
-         delete this.clonedServices[service.id];
-      }
-      else {
-      }
+      this.update(service);
    }
 
    onRowEditCancel(service: Services, index: number) {
-      console.log('editcancel');
-      console.log(service, index);
-      this.service2[index] = this.clonedServices[service.id];
-      delete this.clonedServices[service.id];
+      this.services[index] = this.clonedService[service.id];
+      delete this.clonedService[service.id];
    }
-
-
 }
 
 // current
